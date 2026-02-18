5cdccef3263e4e1b173219a8896839d704dc76fe
Navya-mt <navya.mt@ultiumtech.com>
2026-02-18 12:05:16 +0530

    makefile update

--- Core/Inc/timer.h
+++ Core/Inc/timer.h
@@ -1,7 +1,7 @@
 #ifndef TIMER_H
 #define TIMER_H
 
-#include "stm32f3xx.h"
+#include "stm32f303x8.h"
 
 void timer_init(void);
 void delay_ms(uint32_t ms);
--- Core/Src/clock.c
+++ Core/Src/clock.c
@@ -1,31 +1,31 @@
 #include "clock.h"
-
+ 
 void clock_init(void) {
-    // 1. Start HSI (Internal High Speed oscillator)
-    RCC->CR |= RCC_CR_HSION;
-    while (!(RCC->CR & RCC_CR_HSIRDY));
-
-    // 2. Flash Latency: Essential when running > 24MHz
-    // 72MHz requires 2 wait states
+    // 1. Enable HSE (External High Speed Oscillator)
+    RCC->CR |= RCC_CR_HSEON;
+    while (!(RCC->CR & RCC_CR_HSERDY)); // Wait until HSE is stable
+ 
+    // 2. Flash Latency: 2 wait states for 72MHz
     FLASH->ACR &= ~FLASH_ACR_LATENCY;
-    FLASH->ACR |= FLASH_ACR_LATENCY_1;
-
+    FLASH->ACR |= FLASH_ACR_LATENCY_2;
+ 
     // 3. Configure PLL
-    // Source: HSI / 2 = 4MHz. Multiplier: 4MHz * 18 = 72MHz.
-    RCC->CFGR &= ~(RCC_CFGR_PLLSRC | RCC_CFGR_PLLMUL); 
-    RCC->CFGR |= (RCC_CFGR_PLLSRC_HSI_DIV2 | RCC_CFGR_PLLMUL18);
-
+    // Source: HSE (8MHz). Multiplier: 8MHz * 9 = 72MHz.
+    RCC->CFGR &= ~(RCC_CFGR_PLLSRC | RCC_CFGR_PLLXTPRE | RCC_CFGR_PLLMUL);
+    // RCC_CFGR_PLLSRC_HSE: Selects HSE as source
+    // RCC_CFGR_PLLMUL9: 0x7 value (which is 7 + 2 = 9)
+    RCC->CFGR |= (RCC_CFGR_PLLSRC_HSE_PREDIV  | RCC_CFGR_PLLMUL9);
+ 
     // 4. Set Bus Prescalers
-    // APB1 must not exceed 36MHz -> Set Divider to /2
+    // APB1 must be <= 36MHz. 72MHz / 2 = 36MHz.
     RCC->CFGR |= RCC_CFGR_PPRE1_DIV2;
-
+ 
     // 5. Enable PLL
     RCC->CR |= RCC_CR_PLLON;
     while (!(RCC->CR & RCC_CR_PLLRDY));
-
+ 
     // 6. Switch System Clock to PLL
     RCC->CFGR &= ~RCC_CFGR_SW;
     RCC->CFGR |= RCC_CFGR_SW_PLL;
     while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);
 }
-
